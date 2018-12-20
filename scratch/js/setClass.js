924ccb4ea86c4c363b80c2a9ba75ea5ca7169d0c
Bismillah Frothan <bismillah.frothan@powercoders.org>
2018-12-20 13:44:18 +0100

    java-script

--- scratch/js/setClass.js
+++ scratch/js/setClass.js
@@ -4,7 +4,24 @@ function greet(name) {
   console.log('Hello, ' + name);
 }
 
-function setClass(event, className) {
+function setClass(className) {
   let el = document.getElementById('mypara');
   el.className = className;
 }
+
+document.addEventListener('DOMContentLoaded',function(event) {
+  let makeWarning = document.getElementById('make-warning');
+  makeWarning.addEventListener('click', function(event) {
+    setClass('warning');
+  });
+
+  let makeTip = document.getElementById('make-tip');
+  makeTip.addEventListener('click', function(event) {
+    setClass('tip');
+  });
+
+  let makeClear = document.getElementById('make-clear')
+  makeClear.addEventListener('click', function (event) {
+    setClass('');
+  })
+});
