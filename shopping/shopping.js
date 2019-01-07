afd0e692b150896077b078ecee8d80a1a703c9e8
Bismillah Frothan <bismillah.frothan@powercoders.org>
2019-01-07 14:38:35 +0100

    varify the inputBox value

---
+++ shopping/shopping.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function (event) {
+    document.querySelector('button').addEventListener('click', function (event) {
+        document.getElementById('item');
+        let inputBox = document.getElementById('item');
+        console.log(inputBox.value);
+
+    });
+});
