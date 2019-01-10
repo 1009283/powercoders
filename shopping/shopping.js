74bf42e39798e295ea8d818fea379622379b905e
Bismillah Frothan <bismillah.frothan@powercoders.org>
2019-01-10 14:37:33 +0100

    allow the user to add an item by pressing the Enter key

--- shopping/shopping.js
+++ shopping/shopping.js
@@ -1,8 +1,36 @@
 document.addEventListener('DOMContentLoaded', function (event) {
-    document.querySelector('button').addEventListener('click', function (event) {
-        document.getElementById('item');
-        let inputBox = document.getElementById('item');
-        console.log(inputBox.value);
+  document.querySelector('button').addEventListener('click', function(event) {
+    document.getElementById('item');
+    let inputBox = document.getElementById('item');
+    let li = createNewListItem(inputBox.value);
+    let ul = document.querySelector('ul');
+    ul.appendChild(li);
+  });
 
-    });
+  document.querySelector('input').addEventListener('keyup',function (event) {
+    if (event.key === 'Enter') {
+      let inputBox = document.getElementById('item');
+      let li = createNewListItem(inputBox.value);
+      let ul = document.querySelector('ul');
+      ul.appendChild(li);
+    }
+  });
 });
+
+function createNewListItem(itemName) {
+  let listItem = document.createElement('li');
+  let span = document.createElement('span');
+  let spanText = document.createTextNode(itemName);
+  let deleteButton = document.createElement('button');
+  let buttonText = document.createTextNode('Delete');
+  deleteButton.addEventListener('click', function (event) {
+    console.log('Delete Button is clicked: ' + itemName);
+    listItem.remove();
+  });
+
+  listItem.appendChild(span);
+  span.appendChild(spanText);
+  listItem.appendChild(deleteButton);
+  deleteButton.appendChild(buttonText);
+  return listItem;
+}
