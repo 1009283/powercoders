25476ec6baaa64cf783a65e0837f54bf6310bb51
Bismillah Frothan <bismillah.frothan@powercoders.org>
2019-01-28 11:26:36 +0100

    disabled the clearListButton when the listItemLength is equal to 0

--- shopping/shopping.js
+++ shopping/shopping.js
@@ -1,47 +1,73 @@
 document.addEventListener('DOMContentLoaded', function (event) {
   const inputBox = document.getElementById('item');
   const shoppingList = document.querySelector('ul');
-  const addItemButton = document.querySelector('button');
+  const addItemButton = document.getElementById('add');
+  const clearListButton = document.getElementById('clear');
 
   addItemButton.addEventListener('click', function (event) {
-    if (inputBox.value.trim() !=='') {
-      shoppingList.appendChild(createNewListItem(inputBox.value.trim()));
-      inputBox.value = '';
-      addItemButton.disabled = true;
-    }
+    const trimmedValue = inputBox.value.trim();
+
+    shoppingList.appendChild(createNewListItem(trimmedValue));
+    inputBox.value = '';
     inputBox.focus();
+    clearListButton.disabled = false;
   });
+
+  clearListButton.addEventListener('click', function (event) {
+    const listItems = document.querySelectorAll('li');
+    listItems.forEach(function (element) {
+      element.remove();
+    });
+    clearListButton.disabled = true;
+  });
+
+
 
   inputBox.addEventListener('keyup', function (event) {
-    if (inputBox.value.trim() !=='') {
-      addItemButton.disabled = false;
-      if (event.key === 'Enter') {
-        shoppingList.appendChild(createNewListItem(inputBox.value.trim()));
-        inputBox.value = '';
-      }
-    }
-    if (inputBox.value.trim() === '') {
-      addItemButton.disabled = true;
-    }
-  });
+    const trimmedValue = inputBox.value.trim();
+    addItemButton.disabled = inputBox.value.trim() === '';
 
+    if (trimmedValue === '') {
+      return;
+    }
+
+    if (event.key !== 'Enter') {
+      return;
+    }
+
+    shoppingList.appendChild(createNewListItem(trimmedValue));
+    inputBox.value = '';
+    addItemButton.disabled = true;
+    clearListButton.disabled = false;
+  });
   inputBox.focus();
+  clearListButton.disabled = true;
 });
 
+/**
+ * Creates and returns an 'li' element for inclusion in the shopping list.
+ *
+ * @param {string} itemName Name of the item to add to the list
+ * @returns {HTMLElement} li element
+ */
 function createNewListItem(itemName) {
   const listItem = document.createElement('li');
   const span = document.createElement('span');
   const spanText = document.createTextNode(itemName);
-  const deleteButton = document.createElement('button');
-  const buttonText = document.createTextNode('Delete');
+  const deleteButton = document.createElement('i');
   deleteButton.addEventListener('click', function (event) {
-    console.log('Delete Button is clicked: ' + itemName);
     listItem.remove();
+
+    document.getElementById('clear').disabled =
+        document.querySelectorAll('li').length === 0;
   });
 
   listItem.appendChild(span);
   span.appendChild(spanText);
-  listItem.appendChild(deleteButton);
-  deleteButton.appendChild(buttonText);
+  listItem.appendChild(deleteButton).className = 'fas fa-trash';
   return listItem;
 }
+
+
+
+
