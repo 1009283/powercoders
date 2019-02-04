a487928b42904477647097e9cb6936751fd0a958
Bismillah Frothan <bismillah.frothan@powercoders.org>
2019-02-04 16:04:17 +0100

    - The MVC(ModelViewController) Skeleton

---
+++ shopping-mvc/src/js/ui.js
@@ -0,0 +1,16 @@
+/**
+ * Create the controller to run the application.
+ */
+function domContentLoaded() {
+  new Controller();
+}
+
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', function (event) {
+    domContentLoaded();
+  });
+} else {
+  domContentLoaded();
+}
+
+
