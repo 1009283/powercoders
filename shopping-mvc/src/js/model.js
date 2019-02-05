c28e75879f24d670617b28a2b6a85c5f0f7f3c0a
Bismillah Frothan <bismillah.frothan@powercoders.org>
2019-02-05 15:50:32 +0100

    - delete the dummy data inside the array

--- shopping-mvc/src/js/model.js
+++ shopping-mvc/src/js/model.js
@@ -8,11 +8,7 @@ class Model {
   constructor(controller) {
     console.log('Model initialised');
     /** @private {!ShoppingListItem[]} Items in the list */
-    this.items_ = [
-        new ShoppingListItem('First item', 'q1'),
-        new ShoppingListItem('Second item', 'q2'),
-        new ShoppingListItem('Third item', 'q3')
-    ];
+    this.items_ = [];
 
     /** @private {!View} View for this model. */
     this.view_ = new View(this, controller);
@@ -24,4 +20,13 @@ class Model {
     return this.items_.slice();
   }
 
+  /**
+   * Appends a new item to the list.
+   *
+   * @param item {!ShoppingListItem}
+   */
+  append(item) {
+    this.items_.push(item);
+    this.view_.update();
+  }
 }
