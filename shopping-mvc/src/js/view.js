2e85ae3fc4fea2e1c22fba30d9cabdcd54f2d363
Bismillah Frothan <bismillah.frothan@powercoders.org>
2019-02-05 16:56:48 +0100

    added the addEventlistener for our delete Button

--- shopping-mvc/src/js/view.js
+++ shopping-mvc/src/js/view.js
@@ -16,7 +16,20 @@ class View {
 
     /** @private {!HTMLElement} Shopping list element */
     this.shoppingList_ = document.querySelector('ul');
+
+    /** @private {!HTMLElement} Input widget for items */
+    this.inputBox_ = document.getElementById('item');
+
+    /** @private {!HTMLElement} Input widget for quantities */
+    this.quantiyBox_ = document.getElementById('quantity');
+
+    /** @private {!HTMLElement} Button to add an Item */
+    this.addItemButton_ = document.getElementById('add');
+
+    /** @private {!HTMLElement} Shopping list element */
+    this.addItemButton_.addEventListener('click', () => this.addItem());
   }
+  /** Update the UI with the shopping list contents */
   update() {
     while (this.shoppingList_.firstChild) {
       this.shoppingList_.firstChild.remove();
@@ -25,7 +38,23 @@ class View {
     for (let i = 0; i < this.model_.items.length; i++) {
       const item = this.model_.items[i];
       const listItem = item.toListItem();
+      const deleteButton = listItem.querySelector('i');
+      deleteButton.addEventListener('click', () => this.controller_.deleteItem(i));
       this.shoppingList_.appendChild(listItem);
     }
+
+    this.inputBox_.value = '';
+    this.quantiyBox_.value = '';
+    this.inputBox_.focus();
+  }
+
+  /**
+   * Notifies the controller tho add an item to the list.
+   */
+  addItem() {
+    const trimmedValue = this.inputBox_.value.trim();
+    const trimmedQuantity = this.quantiyBox_.value.trim();
+
+    this.controller_.addItem(trimmedValue, trimmedQuantity);
   }
 }
